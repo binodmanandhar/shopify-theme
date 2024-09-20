5ff69aec142492895c6706c3ab31c68251854421
Binod Manandhar <binodmanandhar7@gmail.com>
2024-09-20 15:02:50 +1000

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./config/*.json",
     "./layout/*.liquid",
+    "./assets/*.liquid",
     "./sections/*.liquid",
     "./snippets/*.liquid",
-    "./templates/customers/*.liquid",
     "./templates/*.liquid",
+    "./templates/*.json",
+    "./templates/customers/*.liquid",
   ],
   theme: {
     extend: {},
